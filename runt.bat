ffaa0586d1767bf511c9ae1a8071f3c158deb4c4
ajborla <ajborla@bigpond.com>
2023-02-07 13:43:47 +1100

    fix(runt.bat): Prevent extraneous output (Windows)
    TYPE command prints file names to STDERR when passed
     multiple file name arguments. To prevent this:
    
    * Redirect STDERR to NUL device, via 2> NUL:
    * Use alternate method via COPY command

--- runt.bat
+++ runt.bat
@@ -15,5 +15,5 @@ rem Version 0.0.1
 rem 03 Apr 2015
 rem ----------------------------------------------------------------------------
 
-type t1.rexx %1.rexx t2.rexx %2.rexx t3.rexx > t.rexx
+copy/v t1.rexx+%1.rexx+t2.rexx+%2.rexx+t3.rexx t.rexx > NUL:
 rexx t.rexx
