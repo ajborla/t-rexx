b699f3dcaad68e78b0cd9727d80fd0865eab98cf
ajborla <ajborla@bigpond.com>
2023-02-08 13:53:50 +1100

    fix(runt*): Pass TAP option to test runner from launch script
    Temporary fix only until launch script(s) refactored.

--- runt.bat
+++ runt.bat
@@ -16,5 +16,5 @@ rem 03 Apr 2015
 rem ----------------------------------------------------------------------------
 
 copy/v t1.rexx+%1.rexx+t2.rexx+%2.rexx+t3.rexx t.rexx > NUL:
-rexx t.rexx
+rexx t.rexx TAP
 exit/b %errorlevel%
