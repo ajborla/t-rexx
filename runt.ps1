c5a5912b4ac823b91ea0100fd5123640e14bd369
ajborla <ajborla@bigpond.com>
2023-02-11 06:25:41 +1100

    feature(runt.ps1): Create Powershell script skeleton
    Seems sensible to add Powershell support. It may, however, prove challenging due
     to the different environments under which Powershell scripts can execute:
    
    * Interactive (Windows)
    * Non-interactive (Windows)
    * Powershell Core (Linux)

---
+++ runt.ps1
@@ -0,0 +1,21 @@
+<#
+.SYNOPSIS
+.DESCRIPTION
+.PARAMETER TestScriptName
+.PARAMETER SourceFileName
+.INPUTS
+.OUTPUTS
+.EXAMPLE
+.LINK
+.NOTES
+#>
+
+[CmdletBinding()]
+param (
+    [String]
+    $TestScriptName,
+
+    [String]
+    $SourceFileName
+)
+
