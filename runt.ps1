07c06d3f2c5a7e5b7381a6c900c357df33d8ed0b
ajborla <ajborla@bigpond.com>
2023-02-11 09:07:08 +1100

    refactor(runt.ps1): Alter file existence checks
    Previous Get-Location-based file existence checks failed if script was executed from
     other than the current directory (in both interactive and non-interactive mode).

--- runt.ps1
+++ runt.ps1
@@ -14,18 +14,32 @@
 param (
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [ValidateScript(
-        {Test-Path -Path (-join (((Get-Location).Path), "\", $_, ".rexx")) -Type Leaf}
-    )]
     [String]
     $TestScriptName,
 
     [Parameter(Mandatory = $true)]
     [ValidateNotNullOrEmpty()]
-    [ValidateScript(
-        {Test-Path -Path (-join (((Get-Location).Path), "\", $_, ".rexx")) -Type Leaf}
-    )]
     [String]
     $SourceFileName
 )
+
+function Get-Script-Location
+{
+    # Script invocation directory: directory in which script resides, not necessarily current directory
+    $scriptInvocation = (Get-Variable MyInvocation -Scope 1).Value
+    return Split-Path $scriptInvocation.MyCommand.Path
+}
+
+Set-Variable -Name ScriptLocation (Get-Script-Location) -Option Private
+Set-Variable -Name TestScriptNamePath (-join ($ScriptLocation, "\", $TestScriptName, ".rexx")) -Option Private
+Set-Variable -Name SourceFileNamePath (-join ($ScriptLocation, "\", $SourceFileName, ".rexx")) -Option Private
+
+# Ensure both test script and source file exist
+if (-not (Test-Path -Path $TestScriptNamePath -Type Leaf)) {
+    throw [System.IO.FileNotFoundException] "Error: Missing test script - $TestScriptNamePath"
+}
+
+if (-not (Test-Path -Path $SourceFileNamePath -Type Leaf)) {
+    throw [System.IO.FileNotFoundException] "Error: Missing source file - $SourceFileNamePath"
+}
 
